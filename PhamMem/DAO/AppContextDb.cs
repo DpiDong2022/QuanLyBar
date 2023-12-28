8a2ee0a0a86d4416499e49c09e47d4d269ade3d7
Dai Dong <phungdaidong1114@gmail.com>
2023-12-28 13:35:57 +0700

    Add project files.

---
+++ PhamMem/DAO/AppContextDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhamMem.DAO {
+    internal class AppContextDb: DbContext {
+        protected AppContextDb() {
+        }
+    }
+}
